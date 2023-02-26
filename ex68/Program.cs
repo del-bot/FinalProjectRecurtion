bea1aff97d866c95f96bfa8d3eb9f0bb8fa0e4f6
Delyash <delyash91@mail.ru>
2023-02-26 00:26:42 -0500

    ex68

---
+++ ex68/Program.cs
@@ -0,0 +1,12 @@
+﻿int AkkermanFunction (int m, int n)
+{
+    if (m == 0) return n+1;
+    if (n == 0) return AkkermanFunction(m-1,1);
+    else return AkkermanFunction((m-1), AkkermanFunction(m, n-1));
+}
+Console.WriteLine("input number M: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number N: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+int res = AkkermanFunction (userNumberM, userNumberN);
+Console.Write(res);
