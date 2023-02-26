be73d3e3bd7612f8d811a436f1cc6785872f6f3b
Delyash <delyash91@mail.ru>
2023-02-26 00:23:34 -0500

    ex64

---
+++ ex64/Program.cs
@@ -0,0 +1,11 @@
+﻿void ShowNumberMN (int m, int n)
+{ 
+    
+    if (n<0)  Console.WriteLine("wrong number");
+    else if (Math.Min(m,n) != Math.Max(m,n)) ShowNumberMN (Math.Min(m,n)+1, Math.Max(m,n));
+    Console.Write($"{Math.Min(m,n)} ");
+    
+}
+Console.WriteLine("input number N: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+ShowNumberMN(1, userNumber);
