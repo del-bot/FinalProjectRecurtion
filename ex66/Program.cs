7e37f72547637c6fe92d6ff083cd1c026d6ce880
Delyash <delyash91@mail.ru>
2023-02-26 00:25:26 -0500

    ex66

---
+++ ex66/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumDigits(int m, int n)
+{
+    if (m > n) return (m + n)*(m - n + 1)/2;
+    if (n > m) return (n + m)*(n - m + 1)/2;
+    return 1;
+}
+Console.WriteLine("input number M: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number N: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+int res = SumDigits(userNumberM, userNumberN);
+Console.Write(res);
